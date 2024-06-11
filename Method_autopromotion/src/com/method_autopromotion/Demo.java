03220f0efa2d96f596dc0f2d57eb49ee50ead84b
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 11:26:58 +0530

    Add files via upload

---
+++ Method_autopromotion/src/com/method_autopromotion/Demo.java
@@ -0,0 +1,10 @@
+package com.method_autopromotion;
+
+public class Demo {
+	
+	void add(double a , double b)
+	{
+		System.out.println("double double add method called");
+	}
+
+}
---
+++ Method_autopromotion/src/com/method_autopromotion/Main.java
@@ -0,0 +1,17 @@
+package com.method_autopromotion;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		Demo d1 = new Demo();
+		d1.add(10,20);
+		d1.add('A', 30);
+		d1.add(10,'A');
+		d1.add('A', 12.2);
+		
+		
+
+	}
+
+}
