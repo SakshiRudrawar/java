2ba04e4a8532fae93032bb5b2559388a0fe2e364
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 11:28:20 +0530

    Add files via upload

---
+++ Method_overloading/src/com/method_overloaing/Demo.java
@@ -0,0 +1,27 @@
+package com.method_overloaing;
+
+public class Demo 
+{
+	
+	void add(int a , int b)
+	{
+		System.out.println(a+b);
+	}
+	
+	void add(int a , double b)
+	{
+		System.out.println(a+b);
+	}
+	
+	void add(double a , int b)
+	{
+		System.out.println(a+b);
+	}
+	
+	void add(double a , double b)
+	{
+		System.out.println(a+b);
+	}
+
+
+}
---
+++ Method_overloading/src/com/method_overloaing/main.java
@@ -0,0 +1,14 @@
+package com.method_overloaing;
+
+public class main {
+
+	public static void main(String[] args) 
+	{
+		Demo d1 = new Demo();
+		d1.add(40, 50);
+		d1.add(2.2 , 3.4);
+		
+
+	}
+
+}
